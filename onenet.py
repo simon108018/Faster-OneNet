dfe62934613ea7abda4a008849f44e252cbda544
simon108018 <simon82509@gmail.com>
2021-09-02 04:38:23 +0800

    9/2

--- onenet.py
+++ onenet.py
@@ -9,8 +9,8 @@ from PIL import Image, ImageDraw, ImageFont
 from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Input
 
-from nets.onenet import centernet
-from utils.utils import centernet_correct_boxes, letterbox_image, nms
+from nets.onenet import onenet
+from utils.utils import onenet_correct_boxes, letterbox_image, nms
 
 
 def preprocess_image(image):
@@ -18,27 +18,28 @@ def preprocess_image(image):
     std = [0.2886383, 0.27408165, 0.27809834]
     return ((np.float32(image) / 255.) - mean) / std
 
-#--------------------------------------------#
+
+# --------------------------------------------#
 #   使用自己训练好的模型预测需要修改3个参数
 #   model_path、classes_path和backbone
 #   都需要修改！
 #   如果出现shape不匹配，一定要注意
 #   训练时的model_path和classes_path参数的修改
-#--------------------------------------------#
-class CenterNet(object):
+# --------------------------------------------#
+class OneNet(object):
     _defaults = {
-        "model_path"        : 'model_data/centernet_resnet50_voc.h5',
-        "classes_path"      : 'model_data/voc_classes.txt',
-        # "model_path"        : 'model_data/centernet_hourglass_coco.h5',
-        # "classes_path"      : 'model_data/coco_classes.txt',
-        "backbone"          : 'resnet50',
-        "input_shape"       : [512,512,3],
-        "confidence"        : 0.3,
+        # "model_path"        : 'model_data/onenet_resnet50_voc.h5',
+        'model_path': 'model_data/onenet_resnet18.h5',
+        # "classes_path"      : 'model_data/voc_classes.txt',
+        "classes_path": 'model_data/coco_classes.txt',
+        "backbone": 'resnet18',
+        "input_shape": [416, 416, 3],
+        "confidence": 0.2,
         # backbone为resnet50时建议设置为True
         # backbone为hourglass时建议设置为False
         # 也可以根据检测效果自行选择
-        "nms"               : True,
-        "nms_threhold"      : 0.3,
+        "nms": True,
+        "nms_threhold": 0.4,
     }
 
     @classmethod
@@ -48,17 +49,17 @@ class CenterNet(object):
         else:
             return "Unrecognized attribute name '" + n + "'"
 
-    #---------------------------------------------------#
-    #   初始化centernet
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
+    #   初始化onenet
+    # ---------------------------------------------------#
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults)
         self.class_names = self._get_class()
         self.generate()
 
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     #   获得所有的分类
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     def _get_class(self):
         classes_path = os.path.expanduser(self.classes_path)
         with open(classes_path) as f:
@@ -66,24 +67,24 @@ class CenterNet(object):
         class_names = [c.strip() for c in class_names]
         return class_names
 
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     #   载入模型
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     def generate(self):
         model_path = os.path.expanduser(self.model_path)
         assert model_path.endswith('.h5'), 'Keras model or weights must be a .h5 file.'
-        
-        #----------------------------------------#
+
+        # ----------------------------------------#
         #   计算种类数量
-        #----------------------------------------#
+        # ----------------------------------------#
         self.num_classes = len(self.class_names)
 
-        #----------------------------------------#
-        #   创建centernet模型
-        #----------------------------------------#
-        self.centernet = centernet(self.input_shape,num_classes=self.num_classes,backbone=self.backbone,mode='predict')
-        self.centernet.load_weights(self.model_path, by_name=True)
-        
+        # ----------------------------------------#
+        #   创建onenet模型
+        # ----------------------------------------#
+        self.onenet = onenet(self.input_shape, num_classes=self.num_classes, backbone=self.backbone, mode='predict')
+        self.onenet.load_weights(self.model_path, by_name=True, skip_mismatch=True)
+
         print('{} model, anchors, and classes loaded.'.format(self.model_path))
 
         # 画框设置不同的颜色
@@ -96,63 +97,69 @@ class CenterNet(object):
 
     @tf.function
     def get_pred(self, photo):
-        preds = self.centernet(photo, training=False)
+        preds = self.onenet(photo, training=False)
         return preds
-    #---------------------------------------------------#
+
+    # ---------------------------------------------------#
     #   检测图片
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     def detect_image(self, image):
         image_shape = np.array(np.shape(image)[0:2])
-        #---------------------------------------------------------#
+        # ---------------------------------------------------------#
         #   给图像增加灰条，实现不失真的resize
-        #---------------------------------------------------------#
-        crop_img = letterbox_image(image, [self.input_shape[0],self.input_shape[1]])
-        #----------------------------------------------------------------------------------#
-        #   将RGB转化成BGR，这是因为原始的centernet_hourglass权值是使用BGR通道的图片训练的
-        #----------------------------------------------------------------------------------#
-        photo = np.array(crop_img,dtype = np.float32)[:,:,::-1]
-        #-----------------------------------------------------------#
+        # ---------------------------------------------------------#
+        crop_img = letterbox_image(image, [self.input_shape[0], self.input_shape[1]])
+        # ----------------------------------------------------------------------------------#
+        #   将RGB转化成BGR，这是因为原始的onenet_hourglass权值是使用BGR通道的图片训练的
+        # ----------------------------------------------------------------------------------#
+        photo = np.array(crop_img, dtype=np.float32)[:, :, ::-1]
+        # -----------------------------------------------------------#
         #   图片预处理，归一化。获得的photo的shape为[1, 512, 512, 3]
-        #-----------------------------------------------------------#
-        photo = np.reshape(preprocess_image(photo),[1,self.input_shape[0],self.input_shape[1],self.input_shape[2]])
-        
+        # -----------------------------------------------------------#
+        photo = np.reshape(preprocess_image(photo), [1, self.input_shape[0], self.input_shape[1], self.input_shape[2]])
+
         preds = self.get_pred(photo).numpy()
-        #-------------------------------------------------------#
-        #   对于centernet网络来讲，确立中心非常重要。
+        # -------------------------------------------------------#
+        #   对于onenet网络来讲，确立中心非常重要。
         #   对于大目标而言，会存在许多的局部信息。
         #   此时对于同一个大目标，中心点比较难以确定。
         #   使用最大池化的非极大抑制方法无法去除局部框
         #   所以我还是写了另外一段对框进行非极大抑制的代码
         #   实际测试中，hourglass为主干网络时有无额外的nms相差不大，resnet相差较大。
-        #-------------------------------------------------------#
+        # -------------------------------------------------------#
         if self.nms:
-            preds = np.array(nms(preds,self.nms_threhold))
+            preds = np.array(nms(preds, self.nms_threhold))
 
-        if len(preds[0])<=0:
+        if len(preds[0]) <= 0:
             return image
 
-        #-----------------------------------------------------------#
+        # -----------------------------------------------------------#
         #   将预测结果转换成小数的形式
-        #-----------------------------------------------------------#
-        preds[0][:,0:4] = preds[0][:,0:4]/(self.input_shape[0]/4)
-        
+        # -----------------------------------------------------------#
+        preds[0][:, 0:4] = preds[0][:, 0:4] / (self.input_shape[0] / 4)
+
         det_label = preds[0][:, -1]
         det_conf = preds[0][:, -2]
         det_xmin, det_ymin, det_xmax, det_ymax = preds[0][:, 0], preds[0][:, 1], preds[0][:, 2], preds[0][:, 3]
-        #-----------------------------------------------------------#
-        #   筛选出其中得分高于confidence的框 
-        #-----------------------------------------------------------#
+        # -----------------------------------------------------------#
+        #   筛选出其中得分高于confidence的框
+        # -----------------------------------------------------------#
         top_indices = [i for i, conf in enumerate(det_conf) if conf >= self.confidence]
         top_conf = det_conf[top_indices]
         top_label_indices = det_label[top_indices].tolist()
-        top_xmin, top_ymin, top_xmax, top_ymax = np.expand_dims(det_xmin[top_indices],-1),np.expand_dims(det_ymin[top_indices],-1),np.expand_dims(det_xmax[top_indices],-1),np.expand_dims(det_ymax[top_indices],-1)
-        
-        #-----------------------------------------------------------#
-        #   去掉灰条部分
-        #-----------------------------------------------------------#
-        boxes = centernet_correct_boxes(top_ymin,top_xmin,top_ymax,top_xmax,np.array([self.input_shape[0],self.input_shape[1]]),image_shape)
+        top_xmin, top_ymin, top_xmax, top_ymax = np.expand_dims(det_xmin[top_indices], -1), \
+                                                 np.expand_dims(det_ymin[top_indices], -1), \
+                                                 np.expand_dims(det_xmax[top_indices], -1), \
+                                                 np.expand_dims(det_ymax[top_indices], -1)
 
-        font = ImageFont.truetype(font='model_data/simhei.ttf',size=np.floor(3e-2 * np.shape(image)[1] + 0.5).astype('int32'))
+        # -----------------------------------------------------------#
+        #   去掉灰条部分
+        # -----------------------------------------------------------#
+        boxes = onenet_correct_boxes(top_ymin, top_xmin, top_ymax, top_xmax,
+                                     np.array([self.input_shape[0], self.input_shape[1]]), image_shape)
+
+        font = ImageFont.truetype(font='model_data/simhei.ttf',
+                                  size=np.floor(3e-2 * np.shape(image)[1] + 0.5).astype('int32'))
 
         thickness = max((np.shape(image)[0] + np.shape(image)[1]) // self.input_shape[0], 1)
 
@@ -177,7 +184,7 @@ class CenterNet(object):
             label_size = draw.textsize(label, font)
             label = label.encode('utf-8')
             print(label, top, left, bottom, right)
-            
+
             if top - label_size[1] >= 0:
                 text_origin = np.array([left, top - label_size[1]])
             else:
@@ -190,6 +197,6 @@ class CenterNet(object):
             draw.rectangle(
                 [tuple(text_origin), tuple(text_origin + label_size)],
                 fill=self.colors[int(c)])
-            draw.text(text_origin, str(label,'UTF-8'), fill=(0, 0, 0), font=font)
+            draw.text(text_origin, str(label, 'UTF-8'), fill=(0, 0, 0), font=font)
             del draw
         return image
